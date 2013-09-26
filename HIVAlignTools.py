e57ed543d978fa974616f8aa03a655349cb3fabc
Judowill <judowill@gmail.com>
2013-09-26 14:58:36 -0400

    commit of stuff from python notebook

--- HIVAlignTools.py
+++ HIVAlignTools.py
@@ -1,10 +1,18 @@
 __author__ = 'will'
-from sklearn.base import BaseEstimator
+import os
+from sklearn.base import BaseEstimator, ClusterMixin
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 from collections import Counter
 import numpy as np
-from GeneralSeqTools import fasta_reader
+import pandas as pd
+from GeneralSeqTools import fasta_reader, fasta_writer
+from subprocess import check_call
+import shlex
+from Bio.Blast import NCBIXML
+from StringIO import StringIO
+from Bio.Blast.Applications import NcbiblastxCommandline, NcbiblastnCommandline
+from tempfile import NamedTemporaryFile
 
 
 class SeqTransformer(BaseEstimator):
@@ -168,16 +176,123 @@ class FindInHIV(BaseEstimator):
         return self.predict(X)
 
 
-class AlignHIV(BaseEstimator):
+def score_seq(known, guess, gapopen=10, gapextend=1):
 
-    def __init__(self, winsize=30, min_k=2, max_k=5):
-        self.winsize = winsize
-        self.min_k = min_k
-        self.max_k = max_k
+    cmd = 'needle -asequence %(cb)s -bsequence %(seq)s -aformat score -gapopen %(go)f -gapextend %(ge)s -outfile %(out)s'
+    with NamedTemporaryFile() as conb_handle:
+        fasta_writer(conb_handle, [('SeqA', known)])
+        conb_handle.flush()
+        os.fsync(conb_handle.fileno())
+        with NamedTemporaryFile() as seq_handle:
+            fasta_writer(seq_handle, [('Seq1', guess)])
+            seq_handle.flush()
+            os.fsync(seq_handle.fileno())
+            with NamedTemporaryFile() as out_handle:
+                param_dict = {'cb': conb_handle.name,
+                              'seq': seq_handle.name,
+                              'out': out_handle.name,
+                              'go': gapopen,
+                              'ge': gapextend
+                              }
+                cmd_list = shlex.split(cmd % param_dict)
+                check_call(cmd_list)
+                for line in out_handle:
+                    parts = line.split()
+                    if len(parts) == 4:
+                        return float(parts[-1][1:-2])
+
+
+def score_seqs(known_seqs, guess_seqs, gapopen=10, gapextend=1):
+
+    score = 0.0
+    for ind in range(known_seqs.shape[0]):
+        score += score_seq(known_seqs[0], guess_seqs[0],
+                           gapopen=gapopen, gapextend=gapextend)
+    return score
+
+
+class BlastAligner(BaseEstimator, ClusterMixin):
+
+    def __init__(self, evalue=10, word_size=2, gapopen=11, gapextend=1,
+                 max_intron_length = 20, tmp_path='/tmp/', result_type='aa',
+                 db_path=NamedTemporaryFile(suffix='.fasta').name, num_threads=1):
+        self.evalue = evalue
+        self.word_size = word_size
+        self.gapopen = gapopen
+        self.gapextend = gapextend
+        self.max_intron_length = max_intron_length
+        self.tmp_path = tmp_path
+        self.result_type = result_type
+        self.db_path = db_path
+        self.num_threads = num_threads
+
+    def _write_seqs(self, X, handle):
+
+        seqs = []
+        for row in range(X.shape[0]):
+            seq = ''.join(X[row])
+            seqs.append(('Seq-%03i' % row, ''.join(l for l in seq if l.isalpha())))
+
+        fasta_writer(handle, seqs)
+        handle.flush()
+        os.fsync(handle.fileno())
 
     def fit(self, X, y):
+        empty_mask = y == 'XX'
 
-        pass
+        with open(self.db_path, 'w') as handle:
+            self._write_seqs(y[~empty_mask], handle)
+        cmd = 'makeblastdb -in %s -dbtype ' % self.db_path
+        if self.result_type == 'aa':
+            cmd += 'prot'
+        else:
+            cmd += 'nucl'
 
+        check_call(shlex.split(cmd))
+        return self
+
+    def predict(self, X):
+
+        if self.result_type == 'aa':
+            blast_cmd = NcbiblastxCommandline
+        else:
+            blast_cmd = NcbiblastnCommandline
+
+        with NamedTemporaryFile(dir=self.tmp_path, delete=True) as fasta_handle:
+            self._write_seqs(X, fasta_handle)
+            blastx_cline = blast_cmd(query=fasta_handle.name,
+                                     db=self.db_path, outfmt=5,
+                                     out='-',
+                                     evalue=self.evalue,
+                                     word_size=self.word_size,
+                                     gapopen=self.gapopen,
+                                     gapextend=self.gapextend,
+                                     max_intron_length=self.max_intron_length,
+                                     num_threads=self.num_threads)
+            stdout, stderr = blastx_cline()
+
+        blast_records = NCBIXML.parse(StringIO(stdout))
+        prots = []
+        for rec in blast_records:
+            for align in rec.alignments:
+                hsp = align.hsps[0]
+                prots.append({'ID': rec.query,
+                              'Seq': hsp.query})
+        blast_out = pd.DataFrame(prots).groupby('ID')['Seq'].first()
+        wanted_out = pd.DataFrame({'ID': ['Seq-%03i' % i for i in range(X.shape[0])],
+                                   'want_seq': [True]*X.shape[0],
+                                   }).groupby('ID')['want_seq'].first()
+        out, _ = blast_out.align(wanted_out, join='right')
+
+        return SeqTransformer().transform(out.fillna('XX').values)
+
+    def score(self, X, y):
+
+        empty_mask = y == 'XX'
+        out_aligns = self.predict(X)
+
+        pos_scores = score_seqs(y[~empty_mask], out_aligns[~empty_mask])
+        bad_scores = score_seqs(out_aligns[empty_mask], out_aligns[empty_mask])
+        return (pos_scores - bad_scores)/y.shape[0]
 
 
