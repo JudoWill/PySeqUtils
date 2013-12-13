e55cf29e3aa63ff5fd4d38359e23ff5d8cbf9716
Will Dampier <judowill@gmail.com>
2013-12-13 10:06:18 -0500

    testing simple retreival of consenus sequences

--- ConSeqs.py
+++ ConSeqs.py
@@ -2,6 +2,9 @@ __author__ = 'will'
 
 import os
 import glob
+from Bio.Alphabet import generic_dna, generic_protein
+
+import GeneralSeqTools
 
 FILEPATH = os.path.abspath(__file__).rsplit(os.sep, 1)[0]
 CONPATH = os.path.join(FILEPATH, 'HIVDBFiles', 'ConSeqFiles')
@@ -15,6 +18,19 @@ def get_region_file(region, typ):
         if (region.lower() in f) and (typ.upper() in f):
             return f
 
-def GetConSeq(region, subtype='B'):
 
-    pass
+def GetConSeq(region, alphabet=generic_dna, subtype='B'):
+
+    if (alphabet == generic_dna) or (alphabet.lower() == 'dna'):
+        path = get_region_file(region, 'dna')
+    elif (alphabet == generic_protein) or (alphabet.lower() == 'pro'):
+        path = get_region_file(region, 'pro')
+    else:
+        raise(TypeError, 'alphabet must be: "dna", "pro", generic_dna, generic_protein')
+
+    wanted_key = 'CONSENSUS_'+subtype
+    with open(path) as handle:
+        for name, seq in GeneralSeqTools.fasta_reader(handle):
+            name = name.split('(')[0]
+            if name == wanted_key:
+                return seq.replace('-', '').replace('$', '')
--- testConSeqs.py
+++ testConSeqs.py
@@ -15,3 +15,20 @@ def test_get_region_file():
         msg = '%s:%s' % (reg, typ)
         expr = 'HIV1_CON_200' in ConSeqs.get_region_file(reg, typ)
         yield ok_, expr, msg
+
+
+def test_get_region_simple():
+
+    checks = [('B', 'ltr', 'dna', 'tGGAaGGGcTaaT'),
+              ('B', 'env', 'dna', 'ATGagaGtGaagG'),
+              ('C', 'env', 'pro', 'MRVRGILRNCQQW'),
+              ('B', 'rev', 'dna', 'ATGGCAGgAAGAAGc'),
+              ('C', 'rev', 'pro', 'MAGRSGDSDEA'),
+              ('B', 'nef', 'dna', 'ATgGgtggcaagt'),
+              ('C', 'nef', 'pro', 'MGGKWSKSSIVGW')]
+
+    for sub, reg, alpha, cor_seq in checks:
+        check_seq = ConSeqs.GetConSeq(reg, subtype=sub, alphabet=alpha)
+        expr = check_seq.lower().startswith(cor_seq.lower())
+        msg = 'Error checking Subtype:%s Region:%s Alpha:%s' % (sub, reg, alpha)
+        yield ok_, expr, msg
