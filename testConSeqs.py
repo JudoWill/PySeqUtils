9bc7b07f754a0ed244e8d903c8756e7992d3a77f
Will Dampier <judowill@gmail.com>
2013-12-16 13:24:46 -0500

    added some tougher regions. Doesn't work yet :/

--- testConSeqs.py
+++ testConSeqs.py
@@ -32,3 +32,16 @@ def test_get_region_simple():
         expr = check_seq.lower().startswith(cor_seq.lower())
         msg = 'Error checking Subtype:%s Region:%s Alpha:%s' % (sub, reg, alpha)
         yield ok_, expr, msg
+
+
+def test_get_region_tough():
+
+    checks = [('B', 'v3', 'pro', 'CTRPNNNTRKSIHIGPGRAFYTTGEIIGDIRQAHC'),
+              ('B', 'gp41', 'pro', 'AVGIGAMFLGFLGAAGS')]
+
+    for sub, reg, alpha, cor_seq in checks:
+        check_seq = ConSeqs.GetConSeq(reg, subtype=sub, alphabet=alpha)
+        expr = check_seq.lower().startswith(cor_seq.lower())
+        msg = 'Error checking Subtype:%s Region:%s Alpha:%s' % (sub, reg, alpha)
+        ok_(expr, msg + ':'+check_seq[:len(cor_seq)])
+        #yield ok_, expr, msg + ':'+check_seq[:len(cor_seq)]
