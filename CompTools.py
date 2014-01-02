83e6fda17990f043332e473c7c580468a8251feb
Will Dampier <judowill@gmail.com>
2014-01-02 14:45:22 -0500

    added a quick function for determing the expected score

--- CompTools.py
+++ CompTools.py
@@ -1,4 +1,8 @@
+from __future__ import division
 __author__ = 'will'
+from collections import Counter
+from itertools import product
+import numpy as np
 
 
 def identity_score(a, b, weight=1, null=0):
@@ -6,3 +10,21 @@ def identity_score(a, b, weight=1, null=0):
 
     return weight if a.lower() == b.lower() else null
 
+
+def expect_score(gAcol, gBcol, score_func=identity_score):
+    """Calculates the expected score under the null-hypothesis that the two
+     inputs come from the same distribution.
+
+     It does this by first grouping both sets into one group and then doing a
+     pairwise comparison using the identity function.
+    """
+
+    counts = Counter(gAcol)+Counter(gBcol)
+
+    scores = []
+    weights = []
+    for (kA, vA), (kB, vB) in product(counts.items(), repeat=2):
+        scores.append(score_func(kA, kB))
+        weights.append(vA*vB)
+
+    return np.average(scores, weights=weights)
--- testCompTools.py
+++ testCompTools.py
@@ -11,3 +11,13 @@ def test_identity_score():
     yield eq_, cis('A', 'a'), 1, 'Missed basic IDENT! with cases'
     yield eq_, cis('A', 'A', weight=10), 10, 'Missed basic IDENT with weight'
     yield eq_, cis('A', 'B', null=-5), -5, 'Missed basic IDENT with weight'
+
+
+def test_expect_score():
+
+    colA = 'AAAAT'
+    colB = 'TCCCA'
+
+    score = CompTools.expect_score(colA, colB)
+    cor_score = 0.38 #by hand
+    eq_(score, cor_score, 'Wrong result compared to the hand-calculation')
